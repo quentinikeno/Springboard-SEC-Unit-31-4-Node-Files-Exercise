19ebf45e61e781b9a2fdc33efdaaa205dcb82f50
Quentin Ikeno <ikenoq@gmail.com>
2022-05-02 18:28:25 -1000

    Install axios.

---
+++ step2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const axios = require("axios");
+const argv = process.argv;
+const path = `${argv[2]}`;
+
+function cat(path) {
+	try {
+		const contents = fs.readFileSync(path, "utf8");
+		console.log(contents);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+}
+
+cat(path);
